459c243539686f92fc2a87c6ad09e921af8fc15c
[Caleb] <[ccullen8806036@conestogac.on.ca]>
2026-05-20 15:03:52 -0400

    port

--- server.ts
+++ server.ts
@@ -7,8 +7,7 @@ import fs from "fs";
 
 async function startServer() {
   const app = express();
-  const port = process.env.PORT || 3000;
-
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
 
